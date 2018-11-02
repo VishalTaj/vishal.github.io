d19c067c661134fa0e38da50ecad1a489ecfe3fb
Vishal Taj PM <VishalTaj@users.noreply.github.com>
2018-11-02 19:24:37 +0800

    Create index.html

---
+++ js/app.js
@@ -0,0 +1,4 @@
+$.getJSON("https://api.stackexchange.com/2.2/users/3422357?site=stackoverflow&key=tMlFoNHGIjMGlsOwtDeDXQ((")
+         .then(function(data) { 
+           console.log(data)
+          })
